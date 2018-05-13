85a872614148bdff02e95c109133c08255cf4cf4
Vin Busquet <computationalcore@gmail.com>
2018-05-13 14:09:33 -0300

    feat: Add ListItem component

---
+++ src/components/ListItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { connect } from 'react-redux';
+// Custom components
+import { CardSection } from './common';
+
+// Action creators
+import * as actions from '../actions';
+
+class ListItem extends Component {
+  render() {
+    const { titleStyle } = styles;
+
+    return (
+      <CardSection>
+        <Text style={titleStyle}>{this.props.library.title}</Text>
+      </CardSection>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
+
+export default connect(null, actions)(ListItem);
